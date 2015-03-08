c87fdd573c36bb1c2d70c3980fb3e3971ee1afbf
Jide Fajobi <wazogear@gmail.com>
2015-03-08 18:17:36 -0400

    removed duplication in code

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -28,17 +28,13 @@ class Reservation < ActiveRecord::Base
     checkin_cars = Reservation.includes(:car).where("end_date >= ?", Date.today)
 
     car_actions_count = checkout_cars.keep_if{|r| r.car.pending_pickup?}.count
-    car_actions_count+= checkin_cars.keep_if{|r| r.car.pending_return?}.count
+    car_actions_count += checkin_cars.keep_if{|r| r.car.pending_return?}.count
   end
   
   # update admin dashboard with new stats
   def update_dashboard
     fleet_utilized_percentage = (StatsService.percent_utilized_within_range(Date.today, Date.tomorrow) * 100).to_i
-    checkout_cars = Reservation.includes(:car).where("start_date <= ?", Date.today)
-    checkin_cars = Reservation.includes(:car).where("end_date >= ?", Date.today)
-
-    car_actions_count = checkout_cars.keep_if{|r| r.car.pending_pickup?}.count
-    car_actions_count+= checkin_cars.keep_if{|r| r.car.pending_return?}.count
+    car_actions_count = Reservation.get_actionable_reservation_count
 
     StatsService.update_admin_dashboard('attention', 'current', car_actions_count )
     StatsService.update_admin_dashboard('fleet_utilized', 'value', fleet_utilized_percentage )
